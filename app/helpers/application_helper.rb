ee8f2b0d615274a9f6d444e6cc8c7ede2ed935a0
sandiegodj <davidjanczyn@gmail.com>
2014-09-20 13:44:53 -0700

    redesign signedin view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def sign_out
+    current_user.update_attribute(:remember_token, User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
 end
